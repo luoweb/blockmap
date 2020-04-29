daeb9e72b0cf98f25014fd587ce6c7c46c4d9eec
kobeee <497189972@qq.com>
2020-04-29 12:41:29 +0800

    private data contract template

---
+++ blockchain/contract_template/private_data_java/src/main/java/org/example/MyPrivateAssetContract.java
@@ -0,0 +1,126 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.example;
+
+import org.hyperledger.fabric.contract.Context;
+import org.hyperledger.fabric.contract.ContractInterface;
+import org.hyperledger.fabric.contract.annotation.Contract;
+import org.hyperledger.fabric.contract.annotation.Default;
+import org.hyperledger.fabric.contract.annotation.Transaction;
+import org.hyperledger.fabric.contract.annotation.Contact;
+import org.hyperledger.fabric.contract.annotation.Info;
+import org.hyperledger.fabric.contract.annotation.License;
+
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Map;
+
+@Contract(name = "MyPrivateAssetContract",
+    info = @Info(title = "MyPrivateAsset contract",
+                description = "My Private Data Smart Contract",
+                version = "0.0.1",
+                license =
+                        @License(name = "Apache-2.0",
+                                url = ""),
+                                contact =  @Contact(email = "private_data_java@example.com",
+                                                name = "private_data_java",
+                                                url = "http://private_data_java.me")))
+@Default
+public class MyPrivateAssetContract implements ContractInterface {
+    String collection = "CollectionOne";
+
+    public  MyPrivateAssetContract() {
+
+    }
+    @Transaction()
+    public boolean myPrivateAssetExists(Context ctx, String myPrivateAssetId) {
+        byte[] buffer = ctx.getStub().getPrivateDataHash(collection, myPrivateAssetId);
+        return (buffer != null && buffer.length > 0);
+    }
+
+    @Transaction()
+    public void createMyPrivateAsset(Context ctx, String myPrivateAssetId) throws UnsupportedEncodingException {
+        boolean exists = myPrivateAssetExists(ctx,myPrivateAssetId);
+        if (exists) {
+            throw new RuntimeException("The asset my private asset "+myPrivateAssetId+" already exists");
+        }
+        MyPrivateAsset privateAsset = new MyPrivateAsset();
+        Map<String, byte[]> transientData = ctx.getStub().getTransient();
+
+        if (transientData.size() == 0 | !transientData.containsKey("privateValue")) {
+            throw new RuntimeException("The privateValue key was not specified in transient data. Please try again.");
+        }
+        privateAsset.privateValue = new String(transientData.get("privateValue"), "UTF-8");
+
+        ctx.getStub().putPrivateData(collection, myPrivateAssetId, privateAsset.toJSONString().getBytes(StandardCharsets.UTF_8));
+    }
+
+    @Transaction()
+    public String readMyPrivateAsset(Context ctx, String myPrivateAssetId) throws UnsupportedEncodingException {
+        boolean exists = myPrivateAssetExists(ctx, myPrivateAssetId);
+        if (!exists) {
+            throw new RuntimeException("The asset my private asset " + myPrivateAssetId + " does not exist");
+        }
+
+        byte[] privateData = ctx.getStub().getPrivateData(collection, myPrivateAssetId);
+        String privateDataString = new String(privateData, "UTF-8");
+
+        return privateDataString;
+    }
+
+    @Transaction()
+    public void updateMyPrivateAsset(Context ctx, String myPrivateAssetId) throws UnsupportedEncodingException {
+        boolean exists = myPrivateAssetExists(ctx, myPrivateAssetId);
+        if (!exists) {
+            throw new RuntimeException("The asset my private asset " + myPrivateAssetId + " does not exist");
+        }
+        MyPrivateAsset privateAsset = new MyPrivateAsset();
+
+        Map<String, byte[]> transientData = ctx.getStub().getTransient();
+
+        if (transientData.size() == 0 | !transientData.containsKey("privateValue")) {
+            throw new RuntimeException("The privateValue key was not specified in transient data. Please try again.");
+        }
+        privateAsset.privateValue = new String(transientData.get("privateValue"), "UTF-8");
+
+        ctx.getStub().putPrivateData(collection, myPrivateAssetId, privateAsset.toJSONString().getBytes(StandardCharsets.UTF_8));
+    }
+
+    @Transaction()
+    public void deleteMyPrivateAsset(Context ctx, String myPrivateAssetId) {
+        boolean exists = myPrivateAssetExists(ctx, myPrivateAssetId);
+        if (!exists) {
+            throw new RuntimeException("The asset my private asset " + myPrivateAssetId + " does not exist");
+        }
+        ctx.getStub().delPrivateData(collection, myPrivateAssetId);
+    }
+
+    @Transaction()
+    public boolean verifyMyPrivateAsset(Context ctx, String myPrivateAssetId, MyPrivateAsset objectToVerify) throws NoSuchAlgorithmException {
+
+        // Convert user provided object into hash
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hashByte = digest.digest(objectToVerify.toJSONString().getBytes(StandardCharsets.UTF_8));
+
+        String hashToVerify = new BigInteger(1, hashByte).toString(16);
+
+        // Get hash stored on the public ledger
+        byte[] pdHashBytes = ctx.getStub().getPrivateDataHash(collection, myPrivateAssetId);
+        if (pdHashBytes.length == 0) {
+            throw new RuntimeException("No private data hash with the key: " + myPrivateAssetId);
+        }
+
+        String actualHash = new BigInteger(1, pdHashBytes).toString(16);
+
+        if (hashToVerify.equals(actualHash)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
---
+++ blockchain/contract_template/private_data_javascript/lib/my-private-asset-contract.js
@@ -0,0 +1,92 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const { Contract } = require('fabric-contract-api');
+const crypto = require('crypto');
+const myCollectionName = 'CollectionOne';
+
+class MyPrivateAssetContract extends Contract {
+
+    async myPrivateAssetExists(ctx, myPrivateAssetId) {
+        const buffer = await ctx.stub.getPrivateDataHash(myCollectionName, myPrivateAssetId);
+        return (!!buffer && buffer.length > 0);
+    }
+
+    async createMyPrivateAsset(ctx, myPrivateAssetId) {
+        const exists = await this.myPrivateAssetExists(ctx, myPrivateAssetId);
+        if (exists) {
+            throw new Error(`The asset my private asset ${myPrivateAssetId} already exists`);
+        }
+
+        const privateAsset = {};
+
+        const transientData = ctx.stub.getTransient();
+        if (transientData.size === 0 || !transientData.has('privateValue')) {
+            throw new Error('The privateValue key was not specified in transient data. Please try again.');
+        }
+        privateAsset.privateValue = transientData.get('privateValue').toString('utf8');
+
+        await ctx.stub.putPrivateData(myCollectionName, myPrivateAssetId, Buffer.from(JSON.stringify(privateAsset)));
+    }
+
+    async readMyPrivateAsset(ctx, myPrivateAssetId) {
+        const exists = await this.myPrivateAssetExists(ctx, myPrivateAssetId);
+        if (!exists) {
+            throw new Error(`The asset my private asset ${myPrivateAssetId} does not exist`);
+        }
+        let privateDataString;
+        const privateData = await ctx.stub.getPrivateData(myCollectionName, myPrivateAssetId);
+        privateDataString = JSON.parse(privateData.toString());
+        return privateDataString;
+    }
+
+    async updateMyPrivateAsset(ctx, myPrivateAssetId) {
+        const exists = await this.myPrivateAssetExists(ctx, myPrivateAssetId);
+        if (!exists) {
+            throw new Error(`The asset my private asset ${myPrivateAssetId} does not exist`);
+        }
+        const privateAsset = {};
+
+        const transientData = ctx.stub.getTransient();
+        if (transientData.size === 0 || !transientData.has('privateValue')) {
+            throw new Error('The privateValue key was not specified in transient data. Please try again.');
+        }
+        privateAsset.privateValue = transientData.get('privateValue').toString('utf8');
+
+        await ctx.stub.putPrivateData(myCollectionName, myPrivateAssetId, Buffer.from(JSON.stringify(privateAsset)));
+    }
+
+    async deleteMyPrivateAsset(ctx, myPrivateAssetId) {
+        const exists = await this.myPrivateAssetExists(ctx, myPrivateAssetId);
+        if (!exists) {
+            throw new Error(`The asset my private asset ${myPrivateAssetId} does not exist`);
+        }
+        await ctx.stub.deletePrivateData(myCollectionName, myPrivateAssetId);
+    }
+
+    async verifyMyPrivateAsset(ctx, myPrivateAssetId, objectToVerify) {
+
+        // Convert provided object into a hash
+        const hashToVerify = crypto.createHash('sha256').update(objectToVerify).digest('hex');
+        const pdHashBytes = await ctx.stub.getPrivateDataHash(myCollectionName, myPrivateAssetId);
+        if (pdHashBytes.length === 0) {
+            throw new Error('No private data hash with the key: ' + myPrivateAssetId);
+        }
+
+        const actualHash = pdHashBytes.toString('hex');
+
+        // Compare the hash calculated (from object provided) and the hash stored on public ledger
+        if (hashToVerify === actualHash) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+}
+
+module.exports = MyPrivateAssetContract;
